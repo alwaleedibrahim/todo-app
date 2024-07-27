c70d88c1855aa119eb0faecd52a8059db7b5b8b6
alwaleedibrahim <alwaleedibramohammed@gmail.com>
2024-07-27 18:57:21 +0300

    Added cors

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 const express = require("express")
 const todosRouter = require("./routes/todos.routes")
 const usersRouter = require("./routes/users.routes")
+const cors = require("cors")
 
 require("dotenv").config()
+
 
 
 require("./models/dbconnection")
 
 const app = express() 
+
+app.use(cors())
 app.use(express.json())
 app.use('/uploads', express.static('uploads'));
 app.use("/todos", todosRouter)
